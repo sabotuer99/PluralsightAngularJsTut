8d241b4427c4ea40c5a9f8ad63101d6c03854fa9
troy <sabotuer99@gmail.com>
2015-03-06 21:39:43 -0700

    Finished up module 2

--- app/controllers/EditEventController.js
+++ app/controllers/EditEventController.js
@@ -3,5 +3,17 @@
  */
 eventsApp.controller('EditEventController',
     function ($scope) {
+        $scope.saveEvent = function(event, newEventForm) {
+            console.log(newEventForm);
+            if(newEventForm.$valid) {
+                window.alert("event" + event.name + " saved!");
+            }
+        };
+
+        $scope.cancelEdit = function() {
+            window.location = "/EventDetails.html";
+        };
+
+
 
     });
